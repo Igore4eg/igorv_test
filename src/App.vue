067d40f7be3bfd67350b9f58d9ae6e9eeee6f85b
Igor Viazmin <prog14@gerchikco.com>
2022-03-23 15:44:44 +0200

    start

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+      <v-app-bar
+        absolute
+        color="light-blue lighten-3"
+        elevate-on-scroll
+        scroll-target="#scrolling-techniques-7"
+      >
+        <v-text-field :rules="rules"></v-text-field>
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+  
+      </v-app-bar>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+};
+</script>
